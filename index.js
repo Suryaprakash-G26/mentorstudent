7e92ddff14e40b8691a4959c7bf86eea894e0a30
Suryaprakash-G26 <gsurya124@gmail.com>
2023-12-03 15:34:26 +0530

    display

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from 'dotenv';
 import { mentorsRouter } from "./routes/mentor.js";
 import { studentsRouter } from "./routes/student.js";
+import { Display } from "./routes/normal.js";
 
 dotenv.config()
 const Port= process.env.port
@@ -9,6 +10,8 @@ const Port= process.env.port
 const app=express()
 
 app.use(express.json());
+
+app.use('/',Display)
 
 app.use('/mentor',mentorsRouter)
 app.use('/student',studentsRouter)
